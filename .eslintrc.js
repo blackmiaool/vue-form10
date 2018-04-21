60a2d84afaf59a6fb651095f10d3bcca04db4676
blackmiaool <blackmiaool@gmail.com>
2018-04-21 12:40:11 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,66 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+  extends: ['plugin:vue/essential', 'airbnb-base'],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // check if imports actually resolve
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'build/webpack.base.conf.js'
+      }
+    }
+  },
+  // add your custom rules here
+  rules: {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      js: 'never',
+      vue: 'never'
+    }],
+    // disallow reassignment of function parameters
+    // disallow parameter object manipulation except for specific exclusions
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'state', // for vuex state
+        'acc', // for reduce accumulators
+        'e' // for e.returnvalue
+      ]
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      optionalDependencies: ['test/unit/index.js']
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'indent':0,
+    "no-console": 0,
+    "arrow-body-style": 0,
+    "arrow-parens": 0,
+    "comma-dangle": 0,
+    "max-len": 0,
+    "indent": 0,
+    "no-loop-func": 0,
+    "no-param-reassign": 0,
+    "quotes":0,
+    "no-mixed-operators":0,
+    "no-plusplus":0,
+    "no-new":0,
+    "no-await-in-loop":0,
+    "consistent-return":0,
+    "func-names":0
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App';
+
+
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+});
