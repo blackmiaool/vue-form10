ec804b36a90d2713581dfe6f0a763181299aed43
blackmiaool <blackmiaool@gmail.com>
2018-05-01 15:36:54 +0800

    update array style

--- src/types/ArrayType.vue
+++ src/types/ArrayType.vue
@@ -1,24 +1,19 @@
 <template>
     <TypeWrapper :form="form">
-        <ol class="list-group" slot="input" style="overflow: auto;margin:0;width:100%;">
+        <ol class="list-group" slot="input">
 
-            <draggable element="el-collapse" :list="model">
-                <li v-for="(item,key) in model" :key="key"
-                    class="list-group-item">
+            <draggable element="li" :list="model" :options="{animation: 150}" v-for="(item,key) in model" :key="key"  class="list-group-item">
                     <AnyType :options="options" :name="key" :sf-form="form.schema.items"
-                        :sf-model.sync="model[key]"
-                    />
-                </li>
+                        :sf-model.sync="model[key]"/>
+           
             </draggable>
         </ol>
     </TypeWrapper>
 </template>
 
 <script>
-
 import draggable from "vuedraggable";
 import Type from "../mixins/type";
-
 
 export default {
     name: "ArrayType",
@@ -40,5 +35,17 @@ export default {
 
 
 <style scoped>
-
+ol {
+    overflow: auto;
+    margin: 0;
+    width: 100%;
+    padding-left: 0;
+    list-style-type: none;
+    margin-right: 0;
+    margin-left: 0;
+    background-color: #fff;
+    border:1px solid #dcdfe6;
+    border-radius: 4px;
+    padding: 10px;    
+}
 </style>
