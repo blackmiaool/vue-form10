091bff1573263bbe50bb7bee5a5da1c081ee4089
blackmiaool <blackmiaool@gmail.com>
2018-05-01 15:37:19 +0800

    move in stdFormObj

--- src/mixins/type.js
+++ src/mixins/type.js
@@ -1,5 +1,28 @@
-import { schemaDefaults } from "json-schema-form-core";
 import TypeWrapper from "../components/TypeWrapper";
+
+// from json-schema-form-core
+function stdFormObj(name, schema, options) {
+    options = options || {};
+
+    // The Object.assign used to be a angular.copy. Should work though.
+    const f = options.global && options.global.formDefaults ?
+        Object.assign({}, options.global.formDefaults) : {};
+    if (options.global && options.global.supressPropertyTitles === true) {
+        f.title = schema.title;
+    } else {
+        f.title = schema.title || name;
+    }
+
+    if (schema.description) { f.description = schema.description; }
+    if (options.required === true || schema.required === true) { f.required = true; }
+    if (schema.maxLength) { f.maxlength = schema.maxLength; }
+    if (schema.minLength) { f.minlength = schema.minLength; }
+    if (schema.readOnly || schema.readonly) { f.readonly = true; }
+    if (schema.minimum) { f.minimum = schema.minimum + (schema.exclusiveMinimum ? 1 : 0); }
+    if (schema.maximum) { f.maximum = schema.maximum - (schema.exclusiveMaximum ? 1 : 0); }
+    f.schema = schema;
+    return f;
+}
 
 export default {
     computed: {
@@ -12,7 +35,7 @@ export default {
             }
         },
         form() {
-            const form = schemaDefaults.stdFormObj(this.name, this.sfForm);
+            const form = stdFormObj(this.name, this.sfForm);
             if (form.schema.format) {
                 form.type = form.schema.format;
             }
