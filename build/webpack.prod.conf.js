6c7638d0ac4a9983cbcd35678bc7dbb9864d63fe
blackmiaool <blackmiaool@gmail.com>
2018-06-24 19:56:45 +0800

    fix webpack

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -109,13 +109,13 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
 
     // copy custom static assets
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, '../static'),
-        to: config.build.assetsSubDirectory,
-        ignore: ['.*']
-      }
-    ])
+    // new CopyWebpackPlugin([
+    //   {
+    //     from: path.resolve(__dirname, '../static'),
+    //     to: config.build.assetsSubDirectory,
+    //     ignore: ['.*']
+    //   }
+    // ])
   ]
 })
 
