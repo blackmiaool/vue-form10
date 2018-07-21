cc6b8616d7c026a9c42934e967cfda207a581255
blackmiaool <blackmiaool@gmail.com>
2018-07-21 11:30:57 +0800

    add missing files

---
+++ src/components/FormatList.vue
@@ -0,0 +1,30 @@
+<template>
+    <el-form>
+        <div v-for="(format,i) in formats" :key="i">
+            <AnyTypePreview :plugin="format" />
+        </div>
+    </el-form>
+</template>
+
+<script>
+import Form10 from "./Form10";
+import AnyTypePreview from "./AnyTypePreview";
+
+export default {
+    name: "FormatList",
+    computed: {
+        formats() {
+            return this.plugins.filter(plugin => {
+                return plugin.form10 && plugin.form10.format;
+            });
+        }
+    },
+    methods: {},
+    mounted() {},
+    props: ["plugins"],
+    components: {
+        Form10,
+        AnyTypePreview
+    }
+};
+</script>
